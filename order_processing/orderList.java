37b6ad82d61876efaabcbc000a131458b933220a
jiexin2006 <jiexin2006@gmail.com>
2026-05-29 22:27:09 +0800

    changed order_processing to use built in queue

--- order_processing/orderList.java
+++ order_processing/orderList.java
@@ -1,56 +1,27 @@
 package order_processing;
-public class orderList {
-    private orderNode head;
-    private orderNode tail;
-    private int size;
 
-    public orderList() {
-        head = null;
-        tail = null;
-        size = 0;
-    }
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class orderList {
+    Queue<order> orderQueue = new LinkedList<>();
+
+    public orderList() {}
 
     public void addOrder(order order) {
-        orderNode newNode = new orderNode(order);
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-            size++;
-        } else {
-            tail.setNext(newNode);
-            tail = newNode;
-            size++;
-        }
+        orderQueue.offer(order);
     }
 
     public void undoOrder() {
-        if (size == 0) {
-            System.out.println("No order to undo");
-        } else {
-            if (size == 1) {
-                head = null;
-                tail = null;
-                size--;
-            } else {
-                orderNode current = head;
-                while (current.getNext() != tail) {
-                    current = current.getNext();
-                }
-                current.setNext(null);
-                tail = current;
-                size--;
-            }
-        }
+        ((LinkedList<order>) orderQueue).pollLast();
     }
 
     public void displayOrders() {
-        if (size == 0) {
+        if (orderQueue.isEmpty()) {
             System.out.println("No orders");
         } else {
-            orderNode current = head;
-            while (current != null) {
-                current.getOrder().displayOrder();
-                current = current.getNext();
+            for (order order : orderQueue) {
+                order.displayOrder();
             }
         }
     }
